b3bd74330b7810b99bc640139ed7d20c7c7e19d2
K.Tashiro <tashikou1682@gmail.com>
2025-07-26 02:53:03 +0900

    Publisherの追加 ID設定機能の改良

--- esp32/ros2esp_arduino_fb/ros2esp_arduino_fb.ino
+++ esp32/ros2esp_arduino_fb/ros2esp_arduino_fb.ino
@@ -11,7 +11,7 @@ TODO:エンコーダの取得部分の追加
 */
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
-// **複数のESPを使用する場合はIDを変更すること** //
+// **複数のESPを使用する場合はIDを変更** //
 #define ID 0
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 
@@ -62,6 +62,8 @@ BluetoothSerial SerialBT;
 #define MD4D 13
 
 rcl_subscription_t subscriber;
+rcl_publisher_t publisher;
+rcl_timer_t timer;
 std_msgs__msg__Int32MultiArray msg;
 rclc_executor_t executor;
 rclc_support_t support;
@@ -80,7 +82,7 @@ volatile int32_t received_data[MAX_ARRAY_SIZE]; // 受信データ
 volatile size_t received_size = 0;              // 受信データのサイズ
 
 // エンコーダのカウント格納用
-int16_t count[3] = {0};
+int16_t count[4] = {0};
 
 #define RCCHECK(fn)             \
     {                           \
@@ -92,6 +94,19 @@ void error_loop() {
     while (1) {
         SerialBT.println("RCL Error!");
         delay(1000);
+    }
+}
+
+// Publisherのコールバック関数
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
+    RCLC_UNUSED(last_call_time);
+    if (timer != NULL) {
+        msg.data.size = 4;
+        msg.data.data[0] = count[0];
+        msg.data.data[1] = count[1];
+        msg.data.data[2] = count[2];
+        msg.data.data[3] = count[3];
+        RCCHECK(rcl_publish(&publisher, &msg, NULL));
     }
 }
 
@@ -111,7 +126,7 @@ void subscription_callback(const void *msgin) {
 }
 
 void setup() {
-    SerialBT.begin("ESP32");
+    SerialBT.begin("ESP32" + String(ID, DEC)); // Bluetoothの初期化
     delay(2000);
 
     // パルスカウンタの定義
@@ -266,14 +281,22 @@ void setup() {
         &subscriber,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray),
-        publisher_topic_name.c_str()));
+        subscriber_topic_name.c_str()));
 
     // Publisherの初期化
     RCCHECK(rclc_publisher_init_default(
         &publisher,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray),
-        subscriber_topic_name.c_str()));
+        publisher_topic_name.c_str()));
+
+    // タイマーの初期化
+    const unsigned int timer_timeout = 10; // 10msごとにコールバックを呼び出す
+    RCCHECK(rclc_timer_init_default(
+        &timer,
+        &support,
+        RCL_MS_TO_NS(timer_timeout),
+        timer_callback));
 
     std_msgs__msg__Int32MultiArray__init(&msg);
     msg.data.data = buffer;
@@ -282,15 +305,14 @@ void setup() {
 
     RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
     RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
     // ピンの初期化 //
-
     // MD DIR
     pinMode(MD1P, OUTPUT);
     pinMode(MD2P, OUTPUT);
     pinMode(MD3P, OUTPUT);
     pinMode(MD4P, OUTPUT);
-
     // MD PWM
     pinMode(MD1D, OUTPUT);
     pinMode(MD2D, OUTPUT);
@@ -312,16 +334,16 @@ void ENC_Read_Task(void *pvParameters) {
     while (1) {
         // SerialBT.println("Reading encoders...");
         pcnt_get_counter_value(PCNT_UNIT_0, &count[0]);
-        pcnt_get_counter_value(PCNT_UNIT_0, &count[1]);
-        pcnt_get_counter_value(PCNT_UNIT_0, &count[2]);
-        pcnt_get_counter_value(PCNT_UNIT_0, &count[3]);
+        pcnt_get_counter_value(PCNT_UNIT_1, &count[1]);
+        pcnt_get_counter_value(PCNT_UNIT_2, &count[2]);
+        pcnt_get_counter_value(PCNT_UNIT_3, &count[3]);
 
         // デバッグ用
         if (received_data[0] == 1) {
             SerialBT.printf("%d, %d, %d, %d\n", count[0], count[1], count[2], count[3]);
         }
-        delay(10);
-        // delay(1); // ウォッチドッグタイマのリセット(必須)
+        // delay(10);
+        delay(1); // ウォッチドッグタイマのリセット(必須)
     }
 }
 
