78f000b56a60f6c4d21c668a6ca400a42d1f959c
takashinosuke <tiramisu-chaohan.8989@docomo.ne.jp>
2025-07-28 17:33:21 +0900

    微調整

--- esp32/robomas/esp32_can.read/esp32_can.read.ino
+++ esp32/robomas/esp32_can.read/esp32_can.read.ino
@@ -1,5 +1,7 @@
 // Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//2025/07/28
+
 #include "Arduino.h"
 #include <CAN.h>
 #include <Ticker.h>
--- esp32/robomas/esp32_can.send/esp32_can.send.ino
+++ esp32/robomas/esp32_can.send/esp32_can.send.ino
@@ -1,5 +1,6 @@
 // Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//2025/07/28
 
 #include <CAN.h>
 #include <Ticker.h>
