361a686f1e1260e6e55c8d143fd7adf4d545aebc
KouTashi <tashikou1682@gmail.com>
2025-07-30 17:25:58 +0900

    mcp2515で回った

--- esp32/robomas/c610_mcp2515/c610_mcp2515.ino
+++ esp32/robomas/c610_mcp2515/c610_mcp2515.ino
@@ -4,15 +4,19 @@
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
-byte txBuf[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-long Pre_millis;
-MCP_CAN CAN0(4); //megaのときは53にする
+byte txBuf[8] = {0};  // 送信用バッファ
+unsigned long Pre_millis = 0;
+
+// CSピンはUNOのデフォルトSPI CSピンの10に設定
+MCP_CAN CAN0(10);
 
 int16_t fmap(double x, double in_min, double in_max, int16_t out_min, int16_t out_max);
 
 void setup()
 {
   Serial.begin(115200);
+
+  // MCP2515初期化。MCP_ANYは受信ID指定なし、速度は1000kbps、クロック8MHz
   if (CAN0.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) == CAN_OK)
   {
     Serial.println("CAN0: Init OK!");
@@ -28,40 +32,58 @@ void setup()
 
 void loop()
 {
-  double motor_ouput_current_A = 0.0;
-  int16_t motor_ouput_current_Byte = fmap(motor_ouput_current_A, 0, 20, 0, 16384);//2バイトに変換
-  txBuf[0] = (motor_ouput_current_Byte >> 8) & 0xFF;//上位バイト
-  txBuf[1] = motor_ouput_current_Byte & 0xFF;//下位バイト
+  double motor_ouput_current_A = 1.0;
 
-  //Send
-  if (millis() - Pre_millis > 20) { // Period: 20ms
-    CAN0.sendMsgBuf(0x200, 0, 8, txBuf);
+  // 0~20Aを0~16384の2バイト値に変換
+  int16_t motor_ouput_current_Byte = fmap(motor_ouput_current_A, 0, 20, 0, 16384);
+
+  txBuf[0] = (motor_ouput_current_Byte >> 8) & 0xFF;  // 上位バイト
+  txBuf[1] = motor_ouput_current_Byte & 0xFF;         // 下位バイト
+  // 残り6バイトは0のまま
+
+  // 20ms周期で送信
+  if (millis() - Pre_millis > 20)
+  {
+    byte sendResult = CAN0.sendMsgBuf(0x200, 0, 8, txBuf);
+    if (sendResult == CAN_OK)
+      Serial.println("CAN send success");
+    else
+      Serial.println("CAN send fail");
+
     Pre_millis = millis();
   }
 
-  //Receive
+  // 受信処理
   if (CAN0.checkReceive() == CAN_MSGAVAIL)
   {
     CAN0.readMsgBuf(&rxId, &len, rxBuf);
-    Serial.print("Recive ID: ");
+    Serial.print("Receive ID: 0x");
     Serial.print(rxId, HEX);
     Serial.print(" Data: ");
 
-    int16_t angle = rxBuf[0] << 8 | rxBuf[1];
-    int16_t rpm = rxBuf[2] << 8 | rxBuf[3];
-    int16_t amp = rxBuf[4] << 8 | rxBuf[5];
-    int8_t  temp = rxBuf[6];
+    if (len >= 7)
+    {
+      int16_t angle = (rxBuf[0] << 8) | rxBuf[1];
+      int16_t rpm = (rxBuf[2] << 8) | rxBuf[3];
+      int16_t amp = (rxBuf[4] << 8) | rxBuf[5];
+      int8_t temp = rxBuf[6];
 
-    Serial.print(angle);
-    Serial.print(",");
-    Serial.print(rpm);
-    Serial.print(",");
-    Serial.print(amp);
-    Serial.print(",");
-    Serial.println(temp);
+      Serial.print(angle);
+      Serial.print(", ");
+      Serial.print(rpm);
+      Serial.print(", ");
+      Serial.print(amp);
+      Serial.print(", ");
+      Serial.println(temp);
+    }
+    else
+    {
+      Serial.println("Invalid data length");
+    }
   }
 }
 
-int16_t fmap(double x, double in_min, double in_max, int16_t out_min, int16_t out_max) {
-  return (x - in_min) * ((double)(out_max - out_min)) / (in_max - in_min) + out_min;
+int16_t fmap(double x, double in_min, double in_max, int16_t out_min, int16_t out_max)
+{
+  return (int16_t)((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
