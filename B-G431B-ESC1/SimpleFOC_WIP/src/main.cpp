fc4f8c4467868a18968ae86fb31677e590505e8f
K.Tashiro <tashikou1682@gmail.com>
2025-11-06 21:42:50 +0900

    BLDCMDでエンコーダー取得

--- B-G431B-ESC1/SimpleFOC_WIP/src/main.cpp
+++ B-G431B-ESC1/SimpleFOC_WIP/src/main.cpp
@@ -1,32 +1,33 @@
 #include <Arduino.h>
 
-void setup() {
-    pinMode(PC6, OUTPUT);
-}
-
-void loop() {
-    delay(1000);
-    digitalWrite(PC6, HIGH);
-    delay(1000);
-    digitalWrite(PC6, LOW);
-}
-
-// #include <SimpleFOC.h>
-
-// Encoder encoder = Encoder(A_HALL1, A_HALL2, 2048, A_HALL3);
-
-// void doA() { encoder.handleA(); }
-// void doB() { encoder.handleB(); }
-// void doIndex() { encoder.handleIndex(); }
-
 // void setup() {
-//     Serial.begin(115200);
-//     encoder.init();
-//     encoder.enableInterrupts(doA, doB, doIndex);
+//     pinMode(PC6, OUTPUT);
 // }
 
 // void loop() {
-//     encoder.update();
-//     Serial.println(encoder.getAngle());
-//     delay(100);
+//     delay(1000);
+//     digitalWrite(PC6, HIGH);
+//     delay(1000);
+//     digitalWrite(PC6, LOW);
 // }
+
+#include <Arduino.h>
+#include <SimpleFOC.h>
+
+Encoder encoder = Encoder(A_HALL1, A_HALL2, 48, A_HALL3);
+
+void doA() { encoder.handleA(); }
+void doB() { encoder.handleB(); }
+void doIndex() { encoder.handleIndex(); }
+
+void setup() {
+    Serial.begin(115200);
+    encoder.init();
+    encoder.enableInterrupts(doA, doB, doIndex);
+}
+
+void loop() {
+    encoder.update();
+    Serial.println(encoder.getAngle());
+    delay(100);
+}
