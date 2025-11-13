776f8e306b724b26e5bea629f132aae5e6cc87e8
K.Tashiro <tashikou1682@gmail.com>
2025-11-13 14:13:02 +0900

    各モードの初期化関数をヘッダー化

---
+++ esp32/ros2io/ros2io_Rev.4/mode_init.h
@@ -0,0 +1,17 @@
+/*====================================================================
+<mode_init.h>
+・各モードの初期化関数の定義ファイル
+Copyright (c) 2025 RRST-NHK-Project. All rights reserved.
+====================================================================*/
+
+#pragma once
+
+#include <Arduino.h>
+
+// 関数のプロトタイプ宣言
+void mode0_init();
+void mode1_init();
+void mode2_init();
+void mode3_init();
+void mode4_init();
+void mode5_init();
