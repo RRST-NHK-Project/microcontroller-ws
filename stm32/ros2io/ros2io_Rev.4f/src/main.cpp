ffc8fb0b2fb170547f6033dd5645459336620f44
K.Tashiro <tashikou1682@gmail.com>
2025-12-12 18:12:50 +0900

    新パッケージ用に調整中

--- stm32/ros2io/ros2io_Rev.4f/src/main.cpp
+++ stm32/ros2io/ros2io_Rev.4f/src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    delay(500);         // PC側がポートを開く時間
+    Serial.write(0x01); // 固定ID送信
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    ;
 }
