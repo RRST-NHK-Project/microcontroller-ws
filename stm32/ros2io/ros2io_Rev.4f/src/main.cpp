13813d9ebd878aee1012820792b8b3614ff51aa9
K.Tashiro <tashikou1682@gmail.com>
2025-12-12 19:16:41 +0900

    IDの送信を常時実行に変更

--- stm32/ros2io/ros2io_Rev.4f/src/main.cpp
+++ stm32/ros2io/ros2io_Rev.4f/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 
+const uint8_t DEVICE_ID = 0x01;
+
 void setup() {
     Serial.begin(115200);
-    delay(500);         // PC側がポートを開く時間
-    Serial.write(0x01); // 固定ID送信
 }
 
 void loop() {
-    ;
+    Serial.write(DEVICE_ID);
+    delay(100);
 }
