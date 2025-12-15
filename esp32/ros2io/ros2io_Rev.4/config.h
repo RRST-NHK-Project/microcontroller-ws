035efc06757f1f8b178043b0d29b82c0a3015a02
K.Tashiro <tashikou1682@gmail.com>
2025-12-15 13:35:31 +0900

    Add Docker installation instructions to config.h

--- esp32/ros2io/ros2io_Rev.4/config.h
+++ esp32/ros2io/ros2io_Rev.4/config.h
@@ -63,3 +63,24 @@ MODEを0に変更することで有効化され、TEST_MODEを変更すること
 
 // 実装途中ですが、WiFi経由でのmicro-ROSエージェント接続も可能です。下記コマンドでエージェント立ち上げを行ってください（Dockerのインストールが必要）。ポート番号は適宜変更すること。
 //  docker run -it --rm --net=host microros/micro-ros-agent:jazzy tcp4 --port 8888
+
+// Dockerのインストールは以下のコマンドで
+/*
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+*/
