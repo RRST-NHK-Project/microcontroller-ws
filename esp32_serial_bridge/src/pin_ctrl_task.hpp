49ad9a77058757b468891b1e7d184052cd498079
KouTashi <tashikou1682@gmail.com>
2026-02-03 21:06:16 +0900

    ピン初期化関数のヘッダー化

--- esp32_serial_bridge/src/pin_ctrl_task.hpp
+++ esp32_serial_bridge/src/pin_ctrl_task.hpp
@@ -9,23 +9,16 @@ Copyright (c) 2025 RRST-NHK-Project. All rights reserved.
 #include <Arduino.h>
 
 // 関数のプロトタイプ宣言
-void Pin_Ctrl_Task(void *); // 入出力タスク
-void Input_Task(void *);    // 入力タスク
-void Output_Task(void *);   // 出力タスク
-void IO_Task(void *);       // 入出力タスク
-void PID_Task(void *);      // PID制御タスク
+void Input_Task(void *);  // 入力タスク
+void Output_Task(void *); // 出力タスク
+void IO_Task(void *);     // 入出力タスク
+void PID_Task(void *);    // PID制御タスク
 void MD_Output();
-void Output_init();
 void Servo_Output();
 void TR_Output();
-void Input_init();
 void ENC_Input();
 void SW_Input();
-void ENCx4_SWx4_init();
-void ENCx2_SWx8_init();
-void ENCx2_init();
 void pid_control();
 void IO_MD_Output();
 void IO_ENC_Input();
 void IO_SW_Input();
-void IO_init();
